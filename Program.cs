5eed0aa8d6854723814c0be75bf4442cb0f9cb61
xsa <xsa24@icloud.com>
2019-08-29 08:12:00 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+
+namespace TaxiFarePrediction {
+    class Program {
+
+        static readonly string _trainDataPath = Path.Combine (Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine (Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+
+        static readonly string _modelPath = Path.Combine (Environment.CurrentDirectory, "Data", "Model.zip");
+
+        static void Main (string[] args) {
+            MLContext mlContext = new MLContext (seed: 0);
+            var model = Train (mlContext, _trainDataPath);
+            Evaluate (mlContext, model);
+
+            TestSinglePrediction (mlContext, model);
+        }
+
+        public static ITransformer Train (MLContext mlContext, string dataPath) {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip> (dataPath, hasHeader : true, separatorChar: ',');
+            var pipeline = mlContext.Transforms.CopyColumns (outputColumnName: "Label", inputColumnName: "FareAmount")
+                .Append (mlContext.Transforms.Categorical.OneHotEncoding (outputColumnName: "VendorIdEncoded", inputColumnName: "VendorId"))
+                .Append (mlContext.Transforms.Categorical.OneHotEncoding (outputColumnName: "RateCodeEncoded", inputColumnName: "RateCode"))
+                .Append (mlContext.Transforms.Categorical.OneHotEncoding (outputColumnName: "PaymentTypeEncoded", inputColumnName: "PaymentType"))
+                .Append (mlContext.Transforms.Concatenate ("Features", "VendorIdEncoded", "RateCodeEncoded", "PassengerCount", "TripTime", "TripDistance", "PaymentTypeEncoded"))
+                .Append (mlContext.Regression.Trainers.FastTree ());
+
+            var model = pipeline.Fit (dataView);
+
+            return model;
+        }
+
+        public static void Evaluate (MLContext mlContext, ITransformer model) {
+            IDataView dataview = mlContext.Data.LoadFromTextFile<TaxiTrip> (_testDataPath, hasHeader : true, separatorChar: ',');
+            var predictions = model.Transform (dataview);
+            var metrics = mlContext.Regression.Evaluate (predictions, "Label", "Score");
+
+            Console.WriteLine ();
+            Console.WriteLine ($"*************************************************");
+            Console.WriteLine ($"*       Model quality metrics evaluation         ");
+            Console.WriteLine ($"*------------------------------------------------");
+            Console.WriteLine ($"*       RSquared Score:      {metrics.RSquared:0.##}");
+            Console.WriteLine ($"*       RSquared Score:      {metrics.RSquared:0.##}");
+        }
+
+        private static void TestSinglePrediction (MLContext mlContext, ITransformer model) {
+            var predictionFunction = mlContext.Model.CreatePredictionEngine<TaxiTrip, TaxiTripFarePrediction> (model);
+
+            var taxiTripSample = new TaxiTrip () {
+                VendorId = "VTS",
+                RateCode = "1",
+                PassengerCount = 1,
+                TripTime = 1140,
+                TripDistance = 3.75f,
+                PaymentType = "CRD",
+                FareAmount = 0 // To predict. Actual/Observed = 15.5
+            };
+
+            var prediction = predictionFunction.Predict (taxiTripSample);
+
+            Console.WriteLine ($"**********************************************************************");
+            Console.WriteLine ($"Predicted fare: {prediction.FareAmount:0.####}, actual fare: 15.5");
+            Console.WriteLine ($"**********************************************************************");
+        }
+    }
+}
+
+/*
+https://docs.microsoft.com/en-us/dotnet/machine-learning/tutorials/predict-prices
+
+In this tutorial, you learned how to:
+
+Prepare and understand the data
+Create a learning pipeline
+Load and transform the data
+Choose a learning algorithm
+Train the model
+Evaluate the model
+Use the model for predictions
+
+ */
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Tutorial: Predict prices using regression with ML.NET
+
